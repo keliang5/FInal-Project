34fe6175942efd73ff1a413ad29134ec3e3d5126
keliang5 <159198151+keliang5@users.noreply.github.com>
2024-05-14 13:54:45 -0700

    Gun and Bullet Finished

--- mob.gd
+++ mob.gd
@@ -1,5 +1,6 @@
 extends CharacterBody2D
 
+var health = 3
 
 @onready var player = get_node("/root/Game/Player")
 
@@ -10,3 +11,8 @@ func _physics_process(delta):
 	var direction = global_position.direction_to(player.global_position)
 	velocity = direction * 300.0
 	move_and_slide()
+
+func take_damage():
+	health -=1
+	if health == 0:
+		queue_free()
